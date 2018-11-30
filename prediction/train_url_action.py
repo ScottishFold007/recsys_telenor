f3cb50f8c319b1a6c3c9b3531595460015603cd3
aksellk <aksellk@student.hist.no>
2018-11-30 17:20:38 +0100

    model params, feeding session, accuracy, minibatch, tensorboard, cuda

--- prediction/train_url_action.py
+++ prediction/train_url_action.py
@@ -5,16 +5,10 @@ import time
 import matplotlib.pyplot as plt
 import math
 import random
+from tensorboardX import SummaryWriter
 
 from url_action_model import Model
-import url_action_preparation as uap
-
-def accuracy(output):
-    correct = 0
-    for pred, target in outputs:
-        if (pred == target):
-            correct += 1
-    return correct, correct/len(outputs)
+import url_action_preparation as uap  
 
 def timeSince(since):
     now = time.time()
@@ -22,68 +16,95 @@ def timeSince(since):
     m = math.floor(s / 60)
     s -= m * 60
     return '%dm %ds' % (m, s)
-    
-def train(model, train_data, loss_function, optimizer):
+
+def test_accuracy(model, test_data, session_size):
+    test_size = len(test_data)
+    test_accuracy = []
+    for url_action_session, target_session in test_data:
+        hidden = None
+        x = url_action_session.to(device)
+        t = target_session.to(device)
+        output, hidden = model(x, hidden)
+
+        # accuracy
+        prediction = torch.argmax(output, dim=1)
+        num_correct = torch.sum(prediction == t)
+        test_accuracy.append(num_correct.item())
+        del x 
+        del t
+    return (sum(test_accuracy)/session_size)/test_size
+
+def train(model, train_data, loss_function, optimizer, session_size, batch_size, decive):
     outputs = []
     total_loss = 0.0
+    accuracy = 0.0
     for url_action_session, target_session in train_data:
         model.zero_grad()
         loss = 0.0
         hidden = None
-        for url_action, target in zip(url_action_session, target_session):
-            #print()
-            output, hidden = model(url_action, hidden)
-            l = loss_function(output, target.unsqueeze(0))
-            loss += l
-        #outputs.append((output.argmax().item(),target.item()))
+        x = url_action_session.to(device)
+        t = target_session.to(device)
+        output, hidden = model(x, hidden)
+        loss = loss_function(output, t)
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
-    return outputs, total_loss
 
+        # accuracy
+        prediction = torch.argmax(output, dim=1)
+        num_correct = torch.sum(prediction == t)
+        accuracy += num_correct.item()/session_size
+        del x
+        del t
+    return outputs, total_loss, accuracy/batch_size
 
+writer = SummaryWriter('logs') 
 train_data, test_data, url_action_set_length, url_set_length = uap.create_dataset()
 
 print('url action set length',url_action_set_length)
 print('url set length',url_set_length)
-model = Model(url_action_set_length, url_set_length)
+print('training size',len(train_data))
+print('test size',len(test_data))
+session_size = len(train_data[0][0])
+print('session size',session_size)
+
+n_iters = 1000
+print_every = 5
+test_every = 100
+all_losses = []
+train_accuracies = []
+test_accuracies = []
+batch_size = 5
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = Model(url_action_set_length, 10, url_set_length, device)
+model.to(device)
 loss_function = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters())
 
-
-n_iters = 100
-print_every = 1
-all_losses = []
-test_every = 100
-all_accuracies = []
 start = time.time()
-
-for iter in range(1, n_iters + 1):
-    random.shuffle(train_data)
-    outputs, loss = train(model, train_data, loss_function, optimizer)
-    all_losses.append(loss)
-
-    if iter % print_every == 0:
-        print('%s (%d %d%%) %.4f' % (timeSince(start), iter, iter / n_iters * 100, loss))
+for i in range(1, n_iters + 1):
+    #random.shuffle(train_data)
+    minibatch = random.sample(train_data, batch_size)
     
-    #correct, acc = accuracy(outputs)
-    #all_accuracies.append(acc)
-    #if iter % test_every == 0:
-        #print('correct: %d accuracy: %.3f' % (correct, acc))
-'''
-test_out = []
-for url, action, one_hot_last_url in test_data:
-    output = model(url, action)
-    outputs.append((output.argmax().item(),one_hot_last_url.item()))
+    outputs, loss, accuracy = train(model, minibatch, loss_function, optimizer, session_size, batch_size, device)
+    writer.add_scalar('Train/Loss', loss, i)
+    writer.add_scalar('Train/Accuracy', accuracy, i)
+    train_accuracies.append(loss)
 
-c, a = accuracy(test_out)
-print('correct: %d accuracy: %.3f' % (c, a))
-'''
+    if i % print_every == 0:
+        print('%s (%d %d%%) %.4f %.4f' % (timeSince(start), i, i / n_iters * 100, loss, accuracy))
+
+    if i % test_every == 0:
+        test_acc = test_accuracy(model, test_data, session_size)
+        test_accuracies.append(test_acc)
+        writer.add_scalar('Test/Accuracy', test_acc, i)
+
+
 plt.figure()
+plt.plot(test_accuracies)
+plt.savefig('img/test_acc.png')
+plt.plot(train_accuracies)
+plt.savefig('img/train_acc.png')
 plt.plot(all_losses)
-plt.show()
-'''
-plt.figure()
-plt.plot(all_accuracies)
-plt.show()
-'''
+plt.savefig('img/loss.png')
--- prediction/url_action_model.py
+++ prediction/url_action_model.py
@@ -3,13 +3,14 @@ import torch.nn as nn
 import torch.autograd as autograd
 
 class Model(nn.Module):
-    def __init__(self, url_action_set_length, url_set_length):
+    def __init__(self, vocabulary_size, embedding_size, output_size, device):
         super(Model, self).__init__()
         #self.hidden_size = hidden_size
-        self.hidden_dim = url_action_set_length
-        self.embedding = nn.Embedding(num_embeddings=url_action_set_length, embedding_dim=url_action_set_length)
-        self.lstm = nn.LSTM(input_size=url_action_set_length, hidden_size=url_action_set_length, num_layers=2)
-        self.linear = nn.Linear(in_features=url_action_set_length, out_features=url_set_length)
+        self.hidden_dim = embedding_size
+        self.device = device
+        self.embedding = nn.Embedding(num_embeddings=vocabulary_size, embedding_dim=embedding_size)
+        self.lstm = nn.LSTM(input_size=embedding_size, hidden_size=embedding_size, num_layers=2)
+        self.linear = nn.Linear(in_features=embedding_size, out_features=output_size)
         self.softmax = nn.Softmax(dim=0)
         self.hidden = self.init_hidden()  
 
@@ -18,13 +19,17 @@ class Model(nn.Module):
     def init_hidden(self):
         # the first is the hidden h
         # the second is the cell  c
-        return (autograd.Variable(torch.zeros(2, 1, self.hidden_dim)), autograd.Variable(torch.zeros(2, 1, self.hidden_dim)))
+        h = autograd.Variable(torch.zeros(2, 1, self.hidden_dim)).to(self.device)
+        c = autograd.Variable(torch.zeros(2, 1, self.hidden_dim)).to(self.device)
+        return (h,c)
 
-    def forward(self, url_action, hidden):
+    def forward(self, session, hidden):
         if hidden==None:
             hidden = self.hidden
-        embedding = self.embedding(url_action)
-        lstm, hidden = self.lstm(embedding.unsqueeze(0).unsqueeze(0),hidden) 
+        embedding = self.embedding(session)
+        x = embedding.view(len(session), 1, -1)
+        lstm, hidden = self.lstm(x,hidden) 
         linear = self.linear(lstm)
         output = self.softmax(linear.squeeze())
-        return output.unsqueeze(0), hidden
+        return output, hidden
+
