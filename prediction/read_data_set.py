f3eb5101b2bfa708e209afb1fc703bb8a47b1006
aksellk <aksellk@student.hist.no>
2018-12-12 14:23:51 +0100

    plot image

--- prediction/read_data_set.py
+++ prediction/read_data_set.py
@@ -5,6 +5,6 @@ dataset = pd.read_csv("./../../../splunk_data_180918_telenor.txt")
 
 # take last 20 000 items
 # was 200
-t = dataset.tail(10000)
+t = dataset.tail(3000)
 
 pickle.dump( t, open( "data_set.p", "wb" ) )
