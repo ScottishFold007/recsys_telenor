298e1e9e2150fbfab39189a434f425ac6a6f4442
aksellk <aksellk@student.hist.no>
2018-12-19 15:47:03 +0100

    embds experiments

--- data_analysis/action2vec.py
+++ data_analysis/action2vec.py
@@ -35,9 +35,9 @@ total_examples = len(corpus)
 print('num sessions', total_examples)
 print('total num events',len(d))    
 
-embedding_size = 10
+embedding_size = 70
 
-model = gensim.models.Word2Vec(corpus, size=embedding_size, window=5, min_count=2, workers=10)
+model = gensim.models.Word2Vec(corpus, size=embedding_size, window=3, min_count=2, workers=10)
 model.train(corpus,total_examples=total_examples,epochs=30)
 
 model_to_CSV(model,embedding_size)
