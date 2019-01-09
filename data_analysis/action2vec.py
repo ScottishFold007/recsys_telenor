5d4721a5674bc1c8384cff2b036776dd71211f21
aksellk <aksellk@student.hist.no>
2019-01-09 16:07:30 +0100

    action2vec on cleaned actions

--- data_analysis/action2vec.py
+++ data_analysis/action2vec.py
@@ -16,10 +16,15 @@ def model_to_CSV(model, embedding_size):
         labels.append(word)
     tokens_df = pd.DataFrame(tokens)
     labels_df = pd.DataFrame(labels)
-    tokens_df.to_csv('word2vec_embeddings/embedding_%d.csv' % embedding_size,sep='\t', index=False, header=None)
-    labels_df.to_csv('word2vec_embeddings/labels_%d.csv' % embedding_size,sep='\t', index=False, header=None)
+    #tokens_df.to_csv('word2vec_embeddings/embedding_%d.csv' % embedding_size,sep='\t', index=False, header=None)
+    #labels_df.to_csv('word2vec_embeddings/labels_%d.csv' % embedding_size,sep='\t', index=False, header=None)
+    tokens_df.to_csv('word2vec_preprocessed_embeddings/embedding_%d.csv' % embedding_size,sep='\t', index=False, header=None)
+    labels_df.to_csv('word2vec_preprocessed_embeddings/labels_%d.csv' % embedding_size,sep='\t', index=False, header=None)
 
 df = pickle.load(open("./../data_set.p", "rb"))
+
+#cleaned data
+df = df[(df['action_cleaned']!='click_on_other')]
 
 # create session UUID
 d = ss.define_session(df)
@@ -27,7 +32,7 @@ d = ss.define_session(df)
 corpus = []
 for uuid, row in d.groupby('UUID'):
     session = []
-    for a in row['action']:
+    for a in row['action_cleaned']:
         session.append(a)
     corpus.append(session)
 
@@ -35,7 +40,7 @@ total_examples = len(corpus)
 print('num sessions', total_examples)
 print('total num events',len(d))    
 
-embedding_size = 70
+embedding_size = 110
 
 model = gensim.models.Word2Vec(corpus, size=embedding_size, window=3, min_count=2, workers=10)
 
