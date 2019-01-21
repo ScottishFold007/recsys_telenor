16d0cce9968f3f10e94cf432c3ccd6cbf35cf381
aksellk <aksellk@student.hist.no>
2019-01-21 12:01:59 +0100

    different datasets

--- prediction/training.py
+++ prediction/training.py
@@ -28,8 +28,7 @@ def test_accuracy(model, x, y, sequence_lengths, batch_size, target_vocabulary_s
     output, _ = model(inputs, sequence_lengths, None)
     output = output.view(batch_size, target_vocabulary_size, session_size)
     accuracy = calc_accuracy(output, targets)
-    del inputs
-    del targets
+    del inputs, targets
     return accuracy
 
 def calc_accuracy(output_distribution, targets):
@@ -47,9 +46,7 @@ def validation(model, x, y, sequence_lengths, batch_size):
     output, _ = model(inputs, sequence_lengths, None)
     output = output.view(batch_size, target_vocabulary_size, session_size) # according to pytorch CE api input format: (minibatch size, #classes, d)
     validation_accuracy = calc_accuracy(output, targets)
-    del inputs 
-    del targets
-    del output
+    del inputs, targets, output 
     return validation_accuracy
 
 def train_minibatch(model, x, y, sequence_lengths, loss_function, optimizer, batch_size, target_vocabulary_size, device):
@@ -63,45 +60,43 @@ def train_minibatch(model, x, y, sequence_lengths, loss_function, optimizer, bat
     optimizer.step()
 
     train_accuracy = calc_accuracy(output, targets)
-    del inputs 
-    del targets
-    del output
+    del inputs, targets, output 
     return loss.item(), train_accuracy
 
-def minibatch_create_train_and_validation(minibatch):
+def split_minibatch(minibatch):
     train_size = int(0.8 * len(minibatch))
-    train = minibatch[0:train_size]
-    validation_set = minibatch[train_size:-1]
+    return minibatch[0:train_size], minibatch[train_size:-1]
+
+def minibatch_create_train_and_validation(minibatch):
+    train, validation_set = split_minibatch(minibatch)
     train_inputs, train_labels, train_lengths = pad_minibatch(train)
     validation_inputs, validation_labels, validation_lengths = pad_minibatch(validation_set)
     return train_inputs, train_labels, train_lengths, validation_inputs, validation_labels, validation_lengths
 
 def pad_minibatch(minibatch):
-    sequences = [] 
-    targets = []
     minibatch.sort(key=lambda x:len(x[0]), reverse=True)
     sequence_lengths = [len(s[0]) for s in minibatch]
-    max_len = max(sequence_lengths)
-    for seq, target in minibatch:
-        pad = np.full((max_len - len(seq)),0)
-        seq = np.concatenate((seq,pad)) 
-        target = np.concatenate((target,pad)) 
-        sequences.append(seq)
-        targets.append(target)
-    return torch.LongTensor(sequences), torch.LongTensor(targets), sequence_lengths
-    
+    sequences, targets = zip(*minibatch)
+    s = torch.nn.utils.rnn.pad_sequence(sequences=sequences, batch_first=True, padding_value=0)
+    t = torch.nn.utils.rnn.pad_sequence(sequences=targets, batch_first=True, padding_value=0)
+    return s, t, sequence_lengths
+   
+#x_train, y_train, x_test, y_test, input_vocabulary_size, target_vocabulary_size = uap.create_dataset_action()
+
+#import imdb_dataset 
+#train_data, test_data, input_vocabulary_size, target_vocabulary_size = imdb_dataset.load_imdb_dataset()
+
+import ecommerce_dataset
+x_train, y_train, x_test, y_test, input_vocabulary_size, target_vocabulary_size = ecommerce_dataset.load_data()
 
 writer = SummaryWriter('tensorboard_logs') 
-train_data, test_data, input_vocabulary_size, target_vocabulary_size = uap.create_dataset_action()
-
 print('input vocabulary length',input_vocabulary_size)
 print('target vocabulary length',target_vocabulary_size)
-print('training size',len(train_data))
-print('test size',len(test_data))
+print('training size',len(x_train))
+print('test size',len(x_test))
 
-n_iters = 50
+n_iters = 100
 print_every = 5
-test_every = 1
 test_batch_size = 80
 train_count = 0
 train_loss_list = []
@@ -118,12 +113,11 @@ model = Model(
     device=device)
 
 model.to(device)
-loss_function = nn.CrossEntropyLoss(ignore_index=0) # ignores zero-padding
+loss_function = nn.CrossEntropyLoss(ignore_index=0) 
 optimizer = optim.Adam(model.parameters())
 
-training_dataset = train_dataset.TrainDataset(train_data)
-testing_dataset = test_dataset.TestDataset(test_data)
-train_loader = torch.utils.data.DataLoader(training_dataset, batch_size=50, collate_fn=minibatch_create_train_and_validation, shuffle=True, num_workers=4, drop_last=True)
+training_dataset = train_dataset.TrainDataset(x_train, y_train)
+train_loader = torch.utils.data.DataLoader(training_dataset, batch_size=20, collate_fn=minibatch_create_train_and_validation, shuffle=True, num_workers=4, drop_last=True)
 
 start = time.time()
 for i in range(1, n_iters + 1):
@@ -165,8 +159,8 @@ for i in range(1, n_iters + 1):
     if i % print_every == 0:
         print('%s (%d %d%%)' % (timeSince(start), i, i / n_iters * 100))
 
-# Test accuracy
-testing_dataset = test_dataset.TestDataset(test_data)
+# test accuracy
+testing_dataset = test_dataset.TestDataset(x_test, y_test)
 test_loader = torch.utils.data.DataLoader(testing_dataset, batch_size=test_batch_size, collate_fn=pad_minibatch, shuffle=False, num_workers=4, drop_last=True)
 test_accuracies_list = []
 for j, minibatch in enumerate(test_loader, 0):
@@ -183,6 +177,8 @@ for j, minibatch in enumerate(test_loader, 0):
 
 test_accuracy = sum(test_accuracies_list)/len(test_accuracies_list)
 print('test accuracy:',test_accuracy)
+
+# ploting
 plt.figure()
 plt.plot(train_accuracies_list)
 plt.savefig('img/train_acc.png')
@@ -193,6 +189,7 @@ plt.figure()
 plt.plot(validation_accuracies_list)
 plt.savefig('img/validation_acc.png')
 
+# save results 
 log_dict = {}
 log_dict['train_loss_list'] = train_loss_list
 log_dict['train_accuracies_list'] = train_accuracies_list
