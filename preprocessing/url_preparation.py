3ea25553fb780712a980dd078e86c5dabe3f2d4d
aksellk <aksellk@student.hist.no>
2019-02-07 14:42:56 +0100

    url prep

--- preprocessing/url_preparation.py
+++ preprocessing/url_preparation.py
@@ -3,7 +3,8 @@ import pandas as pd
 def prepare_urls(df):
     api_keywords = pd.read_csv('../preprocessing/API_keywords.txt')['keywords']
 
+    df['url_cleaned'] = df['url']
     for k in api_keywords:
-        df['url_cleaned'] = df['url'].apply(lambda x: k if k in x else x)
+        df['url_cleaned'] = df['url_cleaned'].apply(lambda x: k if k in x else x)   
     
     return df
